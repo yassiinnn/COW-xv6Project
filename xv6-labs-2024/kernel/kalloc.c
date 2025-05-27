4bb9553697edfb316656ec9b3f71da1e10c905d0
yassiinnn <153564417+yassiinnn@users.noreply.github.com>
2025-05-27 16:25:21 +0300

    Update kalloc.c

--- xv6-labs-2024/kernel/kalloc.c
+++ xv6-labs-2024/kernel/kalloc.c
@@ -8,6 +8,8 @@
 #include "spinlock.h"
 #include "riscv.h"
 #include "defs.h"
+// Reference count array: one count per physical page
+int refcnt[PHYSTOP / PGSIZE];
 
 void freerange(void *pa_start, void *pa_end);
 
@@ -66,6 +68,14 @@ void kfree(void *pa)
     release(&kmem.lock);
     return;
   }
+  void incref(uint64 pa) {
+  refcnt[pa / PGSIZE]++;
+}
+
+void decref(uint64 pa) {
+  refcnt[pa / PGSIZE]--;
+}
+
   release(&kmem.lock);
 
 #ifndef LAB_SYSCALL
