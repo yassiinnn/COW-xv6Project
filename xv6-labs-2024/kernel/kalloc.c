77f93bdfd4df59dba288667482cfde22e7cedede
yassiinnn <153564417+yassiinnn@users.noreply.github.com>
2025-05-27 14:39:52 +0300

    Modify for Copy-On-Write Function

--- xv6-labs-2024/kernel/kalloc.c
+++ xv6-labs-2024/kernel/kalloc.c
@@ -18,9 +18,10 @@ struct run {
   struct run *next;
 };
 
+#define PHYPAGES (PHYSTOP / PGSIZE)
+
 struct {
   struct spinlock lock;
-  #define PHYPAGES (PHYSTOP / PGSIZE)
   int refcnt[PHYPAGES];
   struct run *freelist;
 } kmem;
@@ -29,13 +30,16 @@ void
 kinit()
 {
   initlock(&kmem.lock, "kmem");
+
+  // Zero the reference counts
+  for (int i = 0; i < PHYPAGES; i++) {
+    kmem.refcnt[i] = 0;
+  }
+
   freerange(end, (void*)PHYSTOP);
 }
 
-
-
-void
-freerange(void *pa_start, void *pa_end)
+void freerange(void *pa_start, void *pa_end)
 {
   char *p;
   p = (char*)PGROUNDUP((uint64)pa_start);
@@ -48,20 +52,27 @@ freerange(void *pa_start, void *pa_end)
 // which normally should have been returned by a
 // call to kalloc().  (The exception is when
 // initializing the allocator; see kinit above.)
-void
-kfree(void *pa)
+void kfree(void *pa)
 {
   struct run *r;
 
   if(((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
     panic("kfree");
 
+  acquire(&kmem.lock);
+  int index = (uint64)pa >> PGSHIFT;
+  if (--kmem.refcnt[index] > 0) {
+    // Still in use, do not free
+    release(&kmem.lock);
+    return;
+  }
+  release(&kmem.lock);
 
 #ifndef LAB_SYSCALL
   // Fill with junk to catch dangling refs.
   memset(pa, 1, PGSIZE);
 #endif
-  
+
   r = (struct run*)pa;
 
   acquire(&kmem.lock);
@@ -70,13 +81,10 @@ kfree(void *pa)
   release(&kmem.lock);
 }
 
-
-
 // Allocate one 4096-byte page of physical memory.
 // Returns a pointer that the kernel can use.
 // Returns 0 if the memory cannot be allocated.
-void *
-kalloc(void)
+void *kalloc(void)
 {
   struct run *r;
 
@@ -84,6 +92,7 @@ kalloc(void)
   r = kmem.freelist;
   if(r) {
     kmem.freelist = r->next;
+    kmem.refcnt[(uint64)r >> PGSHIFT] = 1;  // Set initial refcount
   }
   release(&kmem.lock);
 #ifndef LAB_SYSCALL
@@ -93,3 +102,19 @@ kalloc(void)
   return (void*)r;
 }
 
+void incref(uint64 pa) {
+  acquire(&kmem.lock);
+  kmem.refcnt[pa >> PGSHIFT]++;
+  release(&kmem.lock);
+}
+
+void decref(uint64 pa) {
+  kfree((void*)pa);  // kfree handles refcounting now
+}
+
+int refcount(uint64 pa) {
+  acquire(&kmem.lock);
+  int count = kmem.refcnt[pa >> PGSHIFT];
+  release(&kmem.lock);
+  return count;
+}
